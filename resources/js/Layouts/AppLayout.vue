9bae7d2dd1f74d5c4eaa6252341c183023b17362
Chamodya111 <backchamodyapunsara@gmail.com>
2023-07-07 01:54:42 +0530

    first commit

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+
+        <Head :title="title ? title : 'Home'">
+            <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}">
+        </Head>
+
+        <body onload="info_noti()">
+            <!--wrapper-->
+            <div class="wrapper">
+
+                <SideBar />
+
+                <NavBar />
+
+                <slot name="content" />
+
+                <Footer />
+            </div>
+
+        </body>
+    </div>
+</template>
+
+<script>
+import { Head } from '@inertiajs/vue3'
+import NavBar from '@/Components/Main/NavBar.vue'
+import SideBar from '@/Components/Main/SideBar.vue'
+import Footer from '@/Components/Main/Footer.vue'
+
+export default {
+    components: {
+        NavBar,
+        SideBar,
+        Footer,
+    },
+    props: {
+        title: String,
+    },
+}
+</script>
+
+<style lang="css" scoped></style>
---
+++ resources/js/Pages/Home/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <AppLayout>
+        <template #content>
+            <div class="page-wrapper">
+                <div class="page-content">
+
+                    <div class="row">
+                        <h1>Home</h1>
+                    </div>
+
+                </div>
+            </div>
+        </template>
+    </AppLayout>
+</template>
+
+<script>
+import AppLayout from '@/Layouts/AppLayout.vue'
+
+export default {
+    components: {
+        AppLayout,
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
